f4e70243221f68246e23fbbf40f04f671023d86e
Casper Svenning Jensen <casper@svenningjensen.dk>
2014-04-26 12:24:37 +0200

    Improve report

--- kita_website/apps/kitamembership/management/commands/report.py
+++ kita_website/apps/kitamembership/management/commands/report.py
@@ -1,43 +1,47 @@
 # ==encoding: utf-8 ==
 
-from datetime import date
+from datetime import date, timedelta
 import codecs
 
 from django.core.management.base import BaseCommand, CommandError
-from django.contrib.auth.models import User
-
-from selvbetjening.core.members.shortcuts import get_or_create_profile
 
 from kita_website.apps.kitamembership.models import Membership, MembershipState, MembershipType
+from selvbetjening.core.user.models import SUser
+
 
 class Command(BaseCommand):
     args = '<year>'
     help = 'Generates a yearly report for all membership activity'
 
-    def handle(self, *args, **options):
-        members = {}
-        at_date = date(2013, 12, 31)
-        bf_date = date(2012, 12, 31)
+    def handle(self, year, *args, **options):
 
-        fp = codecs.open("report.txt", "w", "utf-8")
+        members = {}
+
+        if year == 'now':
+            at_date = date.today()
+            bf_date = at_date - timedelta(days=356)
+        else:
+            at_date = date(int(year), 12, 31)
+            bf_date = date(int(year)-1, 12, 31)
+
+        fp = codecs.open("report-%s.txt" % year, "w", "utf-8")
 
         membership_income = 0
-        for membership in Membership.objects.filter(bind_date__lt=at_date).filter(bind_date__gt=bf_date):
-            if membership.invoice.is_paid():
+        for membership in Membership.objects.filter(bind_date__lt=at_date).filter(bind_date__gt=bf_date).select_related():
+            if membership.attendee.is_paid():
                 membership_income += membership.price
 
         fp.write(u'=============\n')
         fp.write(u'Medlemskab indtægt %s DKK\n' % membership_income)
         fp.write(u'=============\n')
 
-        for user in User.objects.all():
+        for user in SUser.objects.all():
             state = Membership.objects.get_membership_state(user, at_date)
 
             if state != MembershipState.INACTIVE and state != MembershipState.PASSIVE:
-                profile = get_or_create_profile(user)
-                age = profile.get_age()
+                age = user.get_age()
                 members[age] = members.get(age, [])
-                members[age].append((user, profile, state))
+                members[age].append((user, state))
 
         ranges = [range(0, 7), range(7, 18), range(18, 25), range(25, 120)]
 
@@ -57,17 +61,17 @@ class Command(BaseCommand):
             fp.write(u'=============\n')
 
             for age in r:
-                for user, profile, state in members.get(age, []):
+                for user, state in members.get(age, []):
 
                     fp.write('\n')
                     fp.write(u'%s %s\n' % (user.first_name, user.last_name))
-                    fp.write(u'%s\n' % profile.dateofbirth)
-                    fp.write(u'%s %s %s\n' % (profile.street, profile.city, profile.postalcode))
+                    fp.write(u'%s\n' % user.dateofbirth)
+                    fp.write(u'%s %s %s\n' % (user.street, user.city, user.postalcode))
 
 
                     memberships = []
                     for membership in Membership.objects.filter(user=user).filter(bind_date__lt=at_date).order_by('-bind_date'):
-                        if membership.invoice.is_paid():
+                        if membership.attendee.is_paid():
                             memberships.append(membership)
 
                     def print_memberships(ms):
